17efac85492e13ac0172fd69207ada27d0112033
karye <karye.webb@gmail.com>
2023-09-04 08:07:31 +0200

    Träna på skapa projekt och cw

---
+++ Kapitel-1/HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hej! Vad heter du?");
