730b78b03ac67c433e0fe61e268dc6b59d4293e0
karye <karye.webb@gmail.com>
2023-10-02 14:07:51 +0200

    Tränat på loopar och random

---
+++ Kapitel-4/WhileLoopen/Program.cs
@@ -0,0 +1,34 @@
+﻿// Programmet gissa ett tal
+Console.Clear();
+
+// Slumpa fram ett tal
+Random tärning = new Random();
+int slumptal = tärning.Next(1, 101);     // min=1, max=100
+
+//Console.WriteLine($"Slumptalet är {slumptal}");
+
+
+// Upprepa med en oändlig loop
+while (true)
+{
+    // Be spelaren gissa ett tal 1-100
+    Console.Write("Gissa ett tal 1-100: ");
+    string gissningString = Console.ReadLine(); // Läser in en string
+    int gissning = int.Parse(gissningString);   // Sen konverterar till heltal
+
+    // Kolla om gissningen är rätt
+    if (gissning == slumptal)
+    {
+        Console.WriteLine("Bravo! Du gissade rätt.");
+        break;
+    }
+    else if (gissning > slumptal)
+    {
+        Console.WriteLine("För högt!");
+    }
+    else
+    {
+        Console.WriteLine("För lågt!");
+    }
+
+}
