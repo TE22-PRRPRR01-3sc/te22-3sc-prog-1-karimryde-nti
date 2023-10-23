7a9fc524f3a99b29286eaeb8b53c21093881ff8e
karye <karye.webb@gmail.com>
2023-10-23 09:32:51 +0200

    Tränat på TryParse för stabilt program

---
+++ Kapitel-4/StabiltProgram/Program.cs
@@ -0,0 +1,37 @@
+﻿// Spelet gissa ett tal
+Console.Clear();
+
+// Slumpa fram ett tal 1-100
+Random tärning = new Random();
+int slumptal = tärning.Next(1, 101);
+
+// Inmatningsloop
+int gissning = 0;
+while (true)
+{
+    // Be användaren gissa
+    Console.Write("Gissa ett tal (1-100): ");
+    // Läs in gissningen
+    string gissningString = Console.ReadLine();
+
+    // Testa att konvertera till int
+    bool lyckadKonvertering = int.TryParse(gissningString, out gissning);
+    if (lyckadKonvertering)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Du måste mata in ett tal. Försök igen!");
+    }
+}
+
+// Koll om gissningen är rätt
+if (gissning == slumptal)
+{
+    Console.WriteLine("Bra gissat!");
+}
+else
+{
+    Console.WriteLine("Fel gissat!");
+}
