dafaaf4bf2b33a98a551f09ff44d871ffd83a39a
karye <karye.webb@gmail.com>
2023-10-25 11:06:50 +0200

    Tränat på TryParse() och variablers livslängd

---
+++ Kapitel-4/AreaRaknare/Program.cs
@@ -0,0 +1,33 @@
+﻿// Program för att räkna ut area
+Console.Clear();
+Console.WriteLine("Program för att räkna ut area");
+
+// Skapar variabler för längd och bredd
+double längd = 0;
+double bredd = 0;
+
+// Läs in längden som decimaltal
+bool ärDecimaltal = false;
+while (ärDecimaltal == false)
+{
+    Console.Write("Ange längd i m: ");
+    string längdString = Console.ReadLine();
+
+    // Konvertera från text till decimaltal
+    ärDecimaltal = double.TryParse(längdString, out längd);
+    Console.WriteLine($"TryParse säger {ärDecimaltal} och konvertering blir {längd}");
+}
+
+// Läs in bredden som decimaltal
+while (ärDecimaltal == false)
+{
+    Console.Write("Ange bredd i m: ");
+    string breddString = Console.ReadLine();
+
+    // Konvertera från text till decimaltal
+    ärDecimaltal = double.TryParse(breddString, out bredd);
+    Console.WriteLine($"TryParse säger {ärDecimaltal} och konvertering blir {bredd}");
+}
+
+// Skriv ut arean
+Console.WriteLine($"Arean blir {längd * bredd}");
---
+++ Kapitel-4/Ovning1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Skapa ett program som ber användaren att mata in ett tal.
+// Programmet skall skriva om det inmattade är ett heltal eller decimaltal.
+Console.WriteLine("Program som berättar om inmatat är heltal eller decimaltal");
+
+// Inmatning
+Console.WriteLine("Ange ett tal: ");
+string talString = Console.ReadLine();
+
+// Kolla om det är ett heltal
+int heltal = 0;
+bool ärHeltal = int.TryParse(talString, out heltal);
+
+if (ärHeltal)
+{
+    Console.WriteLine("Du har matat in ett heltal!");
+}
+
+// Kolla om det är ett decimaltal
+int decimaltal = 0;
+bool ärDecimaltal = int.TryParse(talString, out decimaltal);
+
+if (ärDecimaltal)
+{
+    Console.WriteLine("Du har matat in ett decimaltal!");
+}
