b6ef7e1e1244e9cbf3a784bd588b2151c7544a26
karye <karye.webb@gmail.com>
2024-01-10 11:07:52 +0100

    Övatpå array och loopar

---
+++ Kapitel-5/Labyrint/Program.cs
@@ -0,0 +1,41 @@
+﻿// Labyrint, ett konsolsspel
+Console.Clear();
+
+// Aktivera UTF8 för att visa Emoji
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// Skapar en labyrint 10 x 10
+int[,] labyrint = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, // y = 0
+    {1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+    {1, 1, 1, 1, 0, 0, 0, 1, 0, 1},
+    {1, 0, 0, 0, 0, 1, 1, 1, 0, 1},
+    {1, 0, 1, 1, 1, 0, 0, 1, 0, 1},
+    {1, 0, 1, 0, 0, 1, 0, 1, 0, 1},
+    {1, 0, 1, 0, 1, 0, 0, 1, 0, 1},
+    {1, 0, 1, 0, 1, 0, 1, 1, 0, 1},
+    {1, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+};
+
+// Loopa igenom raderna: y=0 -> y=9
+for (int y = 0; y < 10; y++)
+{
+    // Rita ut varje på raden
+    for (int x = 0; x < 10; x++)    // x -> 0-9
+    {
+        // Flyta markören 
+        Console.SetCursorPosition(x * 2, y);
+
+        // Hämta vad som finns i den positionen
+        int ruta = labyrint[x, y];
+
+        // Skriv ut det som finns i den positionen
+        if (ruta == 1)
+        {
+            Console.Write("⬛");
+        }
+    }
+}
+
+
