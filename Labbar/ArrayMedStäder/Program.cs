1201e45753c8bf9060611e6fecb38de50ae054d3
karye <karye.webb@gmail.com>
2024-01-15 16:44:38 +0100

    Lärforum träna array och loopar

---
+++ Labbar/ArrayMedStäder/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+Console.Clear();
+
+// Skapa en tom string-lista som heter städer med 10 platser.
+string[] städer = new string[10];
+
+// Skapa en loop där du varje gång loopen körs hämtar en string från användaren med Console.ReadLine().
+// Svar 1: läs in alla 10 städer
+for (int i = 0; i < städer.Length; i++)
+{
+    Console.Write("Ange en stad: ");
+    städer[i] = Console.ReadLine();
+}
+
+// Svar 2: låt användaren bestämma hur många han vill mata in
+for (int i = 0; i < städer.Length; i++)
+{
+    Console.Write("Ange en stad: ");
+    städer[i] = Console.ReadLine();
+
+    Console.Write("Vill du mata in en till? (j/n) ");
+    if (Console.ReadLine() != "j")
+    {
+        break;
+    }
+}
+
+// Svar 3: låt användaren bestämma i förväg antal namn att mata in
+Console.Write("Hur många namn vill du mata in? ");
+int antal = int.Parse(Console.ReadLine());
+for (int i = 0; i < antal; i++)
+{
+    Console.Write("Ange en stad: ");
+    städer[i] = Console.ReadLine();
+}
---
+++ Labbar/ArrayOchLoopar/Program.cs
@@ -0,0 +1,30 @@
+﻿// Repetition
+Console.Clear();
+
+// Skapa en string-array med namnen på fem olika spel.
+string[] spellista = {"s1", "s2", "s3", "s4", "s5"}; // index = 0, 1, 2, 3, 4
+
+// Skapa en for-loop (eller foreach-loop) som skriver ut namnet på varje spel på en egen rad.
+foreach (var spel in spellista)
+{
+    Console.WriteLine(spel);
+}
+
+// Skapa en string-array med namnen på fem av dina klasskamrater.
+string[] namnlista = {"Victor", "Ella", "Olle", "Cesar", "Ivar"}; // index = 0, 1, 2, 3, 4
+
+// Skapa en int-array med fem valfria värden mellan 0 och 10.
+int[] tallista = {1, 4, 2, 7, 8}; // index = 0, 1, 2, 3, 4
+
+// Skapa en for-loop som skriver ut namnen på kompisarna, en per rad
+for (int i = 0; i < namnlista.Length; i++)
+{
+    Console.WriteLine(namnlista[i]);
+}
+
+// Komplettera for-loopen så att den på varje rad skriver ut något liknande "Pelle ger CounterStrike 99 betyget 5".
+for (int i = 0; i < namnlista.Length; i++)
+{
+    Console.WriteLine($"{namnlista[i]} ger {spellista[i]} betyget {tallista[i]}");
+}
+
