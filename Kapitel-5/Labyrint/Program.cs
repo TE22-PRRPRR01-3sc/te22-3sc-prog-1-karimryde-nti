916a7df4608a0267f3a335bb90ba2b1244ab4ee1
karye <karye.webb@gmail.com>
2024-03-06 09:53:53 +0100

    Fixat fel

--- Kapitel-5/Labyrint/Program.cs
+++ Kapitel-5/Labyrint/Program.cs
@@ -28,7 +28,7 @@ for (int y = 0; y < 10; y++)
         Console.SetCursorPosition(x * 2, y);
 
         // Hämta vad som finns i den positionen
-        int ruta = labyrint[x, y];
+        int ruta = labyrint[y, x];
 
         // Skriv ut det som finns i den positionen
         if (ruta == 1)
@@ -37,7 +37,7 @@ for (int y = 0; y < 10; y++)
         }
         else
         {
-            Console.Write("  ");
+            Console.Write("⬜");
         }
     }
 }
@@ -49,33 +49,42 @@ Console.CursorVisible = false;
 int figurX = 1;
 int figurY = 1;
 
+Console.SetCursorPosition(figurX * 2, figurY);
+Console.Write("☕");
+
 // Interaktionsloop, där vi lyssnar på tangentnedtryckningar
 while (true)
 {
     // Läs in tangenten
     ConsoleKeyInfo tangent = Console.ReadKey(true);
 
+    // För att komma ihåg positionen innan vi flyttar
+    int gamlaPosX = figurX;
+    int gamlaPosY = figurY;
+
     // Flytta en figur med piltangenterna
-    if (tangent.Key == ConsoleKey.RightArrow)
+    if (tangent.Key == ConsoleKey.RightArrow && labyrint[figurY, figurX + 1] == 0)
     {
         figurX += 1; // Högerpil flyttar +1 
     }
-    else if (tangent.Key == ConsoleKey.LeftArrow)
+    else if (tangent.Key == ConsoleKey.LeftArrow && labyrint[figurY, figurX - 1] == 0)
     {
         figurX -= 1; // Högerpil flyttar -1 
     }
-    else if (tangent.Key == ConsoleKey.UpArrow)
+    else if (tangent.Key == ConsoleKey.UpArrow && labyrint[figurY - 1, figurX] == 0)
     {
         figurY -= 1; // Uppåtpil flyttar -1
     }
-    else if (tangent.Key == ConsoleKey.DownArrow)
+    else if (tangent.Key == ConsoleKey.DownArrow && labyrint[figurY + 1, figurX] == 0)
     {
         figurY += 1; // Uppåtpil flyttar +1
     }
 
-    // Flytta markören (cursor)
-    Console.SetCursorPosition(figurX * 2, figurY);
+    // Återställ den gamla positionen
+    Console.SetCursorPosition(gamlaPosX * 2, gamlaPosY);
+    Console.Write("⬜");
 
-    // Rita ut en figur
+    // Flytta markören (cursor) och rita ut figur
+    Console.SetCursorPosition(figurX * 2, figurY);
     Console.Write("☕");
 }
